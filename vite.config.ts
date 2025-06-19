2b6b75ec61406e512ff8934a4d20436473e74972
Fachri Hawari <fachri.hawari@gmail.com>
2025-06-19 15:12:02 +0700

    feat: add PWA support with asset generation and icons
    - Updated package.json to include PWA dependencies and a script for generating PWA assets.
    - Added new PWA-related images: apple-touch-icon, favicon, logo, maskable icon, and various sizes of PWA icons.
    - Configured Vite to use the PWA plugin with an auto-update registration type and specified the manifest with icon details.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,40 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte(), tailwindcss()],
+  plugins: [
+    svelte(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        "icons": [
+          {
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      }
+    })
+  ],
 });
