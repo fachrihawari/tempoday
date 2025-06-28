18be52505fc51a5a8caa674ee05887dfdc77bcbc
Fachri Hawari <fachri.hawari@gmail.com>
2025-06-28 09:31:35 +0700

    Updated Header.svelte

--- src/components/ui/Header.svelte
+++ src/components/ui/Header.svelte
@@ -4,5 +4,4 @@ import Settings from '../Settings.svelte';
 
 <div class="flex items-center justify-between p-4 border-b border-gray-200">
   <h1 class="text-2xl font-bold text-gray-900">TempoDay</h1>
-  <Settings />
 </div>
