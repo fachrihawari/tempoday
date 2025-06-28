dc854a92242f9683d75019d0c96200faf8a56bce
Fachri Hawari <fachri.hawari@gmail.com>
2025-06-28 11:41:47 +0700

    Updated Settings.svelte

--- src/pages/Settings.svelte
+++ src/pages/Settings.svelte
@@ -97,7 +97,7 @@ import PageHeader from '../components/ui/PageHeader.svelte';
       
       <div class="flex items-center gap-4 p-4 bg-gray-50 rounded-lg">
         <div class="w-16 h-16 rounded-2xl flex items-center justify-center shadow-lg">
-          <img src='/logo.png' class="w-12 h-12" />
+          <img src='/logo.png' class="w-10 h-10" />
         </div>
         <div class="flex-1">
           <h3 class="font-semibold text-gray-900 text-lg">TempoDay</h3>
