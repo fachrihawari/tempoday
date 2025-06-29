e9f2aa34309c15f0ff18f21984cfde2cf351db48
Fachri Hawari <fachri.hawari@gmail.com>
2025-06-29 10:33:27 +0700

    Updated BackupRestore.svelte

--- src/components/BackupRestore.svelte
+++ src/components/BackupRestore.svelte
@@ -243,7 +243,7 @@ function getBackupMethodTitle(method: string): string {
     class="!p-4 !text-left !justify-start"
   >
     {#snippet children()}
-      <div class="flex items-center gap-4">
+      <div class="flex items-center gap-4 w-full">
         <div class="w-12 h-12 bg-green-100 rounded-xl flex items-center justify-center">
           <Icon name="trending-up" class="text-green-600" size="lg" />
         </div>
